b7c4d53c61396bc8bc0ed96a49794c4c749fd46b
JeetLi <jeetwork168@gmail.com>
2024-09-17 17:44:47 +0300

    ref: calculator optimize

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -3,6 +3,29 @@
 import { useState } from "react";
 import s from "./calculator.module.scss";
 
+const btnsArr = [
+  { value: "C", color: "#D2D3DA" },
+  { value: "+/-", color: "#D2D3DA" },
+  { value: "%", color: "#D2D3DA" },
+  { value: "/", color: "#3B75A2" },
+  { value: "7", color: "#FFFFFF" },
+  { value: "8", color: "#FFFFFF" },
+  { value: "9", color: "#FFFFFF" },
+  { value: "*", color: "#3B75A2" },
+  { value: "4", color: "#FFFFFF" },
+  { value: "5", color: "#FFFFFF" },
+  { value: "6", color: "#FFFFFF" },
+  { value: "-", color: "#3B75A2" },
+  { value: "1", color: "#FFFFFF" },
+  { value: "2", color: "#FFFFFF" },
+  { value: "3", color: "#FFFFFF" },
+  { value: "+", color: "#3B75A2" },
+  { value: ".", color: "#FFFFFF" },
+  { value: "0", color: "#FFFFFF" },
+  { value: "<X", color: "#FFFFFF" },
+  { value: "=", color: "#3B75A2" },
+];
+
 const Calculator = () => {
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
@@ -16,49 +39,31 @@ const Calculator = () => {
   };
 
   const handleButtonClick = (value: string) => {
-    if (value === "=") {
-      try {
-        const evaluatedResult = eval(input);
-        setResult(formatNumber(evaluatedResult));
-      } catch {
-        setResult("Ошибка");
-      }
-    } else if (value === "C") {
-      setInput("");
-      setResult("");
-    } else if (value === "+/-") {
-      setInput(input.startsWith("-") ? input.slice(1) : `-${input}`);
-    } else if (value === "%") {
-      setInput((parseFloat(input) / 100).toString());
-    } else if (value === "<X") {
-      setInput(input.slice(0, -1));
-    } else {
-      setInput(input + value);
+    switch (value) {
+      case "=":
+        try {
+          setResult(formatNumber(eval(input)));
+        } catch {
+          setResult("Ошибка");
+        }
+        break;
+      case "C":
+        setInput("");
+        setResult("");
+        break;
+      case "+/-":
+        setInput(input.startsWith("-") ? input.slice(1) : `-${input}`);
+        break;
+      case "%":
+        setInput((parseFloat(input) / 100).toString());
+        break;
+      case "<X":
+        setInput(input.slice(0, -1));
+        break;
+      default:
+        setInput(input + value);
     }
   };
-
-  const btnsArr = [
-    { value: "C", color: "#D2D3DA" },
-    { value: "+/-", color: "#D2D3DA" },
-    { value: "%", color: "#D2D3DA" },
-    { value: "/", color: "#3B75A2" },
-    { value: "7", color: "#FFFFFF" },
-    { value: "8", color: "#FFFFFF" },
-    { value: "9", color: "#FFFFFF" },
-    { value: "*", color: "#3B75A2" },
-    { value: "4", color: "#FFFFFF" },
-    { value: "5", color: "#FFFFFF" },
-    { value: "6", color: "#FFFFFF" },
-    { value: "-", color: "#3B75A2" },
-    { value: "1", color: "#FFFFFF" },
-    { value: "2", color: "#FFFFFF" },
-    { value: "3", color: "#FFFFFF" },
-    { value: "+", color: "#3B75A2" },
-    { value: ".", color: "#FFFFFF" },
-    { value: "0", color: "#FFFFFF" },
-    { value: "<X", color: "#FFFFFF" },
-    { value: "=", color: "#3B75A2" },
-  ];
 
   const renderBtn = (value: string, color: string, span?: number) => (
     <div
